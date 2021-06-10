0fc99a60361d99f4bdfcd47ed25e7a26414bdb4f
Azaliia Khasanova <azaliia.khasanova@outlook.com>
2021-06-09 20:20:32 -0700

    Added test_add_contact Refactored test test_add_contact Added class Contact for the data

---
+++ contact.py
@@ -0,0 +1,27 @@
+class Contact:
+
+    def __init__(self, fname, mname, lname, nick, title, comp, addr, home, mobile, work, fax, email1, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, secaddr, secphone, note):
+        self.fname=fname
+        self.mname=mname
+        self.lname=lname
+        self.nick=nick
+        self.title=title
+        self.comp=comp
+        self.addr=addr
+        self.home=home
+        self.mobile=mobile
+        self.work=work
+        self.fax=fax
+        self.email1=email1
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage=homepage
+        self.bday=bday
+        self.bmonth=bmonth
+        self.byear=byear
+        self.aday=aday
+        self.amonth=amonth
+        self.ayear=ayear
+        self.secaddr=secaddr
+        self.secphone=secphone
+        self.note=note
